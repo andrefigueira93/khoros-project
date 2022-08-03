3944377f826cfe6816e079a62892fce7c9aa4501
Felipe Freitas <andrefigueira93@gmail.com>
2022-08-03 14:30:03 -0300

    Temporary fix

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -10,39 +10,39 @@ export default function Page({ story }) {
   return <StoryblokComponent blok={story.content} />;
 }
 
-export async function getStaticProps({ params, preview }) {
-  const slug = params.slug ? params.slug.join('/') : 'home';
+// export async function getStaticProps({ params, preview }) {
+//   const slug = params.slug ? params.slug.join('/') : 'home';
 
-  const getData = await fetch(
-    `${
-      process.env.NODE_ENV === 'development'
-        ? process.env.NEXT_PUBLIC_DEV_URL
-        : process.env.NEXT_PUBLIC_PROD_URL
-    }/api/storyblok?slug=${slug}`
-  );
-  const data = await getData.json();
+//   const getData = await fetch(
+//     `${
+//       process.env.NODE_ENV === 'development'
+//         ? process.env.NEXT_PUBLIC_DEV_URL
+//         : process.env.NEXT_PUBLIC_PROD_URL
+//     }/api/storyblok?slug=${slug}`
+//   );
+//   const data = await getData.json();
 
-  return {
-    props: {
-      story: data ? data.story : false,
-      key: data ? data.story.id : false,
-      preview: preview || false,
-    },
-    revalidate: 20,
-  };
-}
+//   return {
+//     props: {
+//       story: data ? data.story : false,
+//       key: data ? data.story.id : false,
+//       preview: preview || false,
+//     },
+//     revalidate: 20,
+//   };
+// }
 
-export async function getStaticPaths() {
-  const getPaths = await fetch(
-    `${
-      process.env.NODE_ENV === 'development'
-        ? process.env.NEXT_PUBLIC_DEV_URL
-        : process.env.NEXT_PUBLIC_PROD_URL
-    }/api/storyblok`
-  );
-  const paths = await getPaths.json();
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const getPaths = await fetch(
+//     `${
+//       process.env.NODE_ENV === 'development'
+//         ? process.env.NEXT_PUBLIC_DEV_URL
+//         : process.env.NEXT_PUBLIC_PROD_URL
+//     }/api/storyblok`
+//   );
+//   const paths = await getPaths.json();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
