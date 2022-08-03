6542dc3a0d17a9518a3a7c5861ae645c5006dff8
Felipe Freitas <andrefigueira93@gmail.com>
2022-08-03 10:56:09 -0300

    Next CORS config

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -12,26 +12,28 @@ export default function Page({ story }) {
   );
 }
 
-// export async function getStaticProps({ params }) {
-//   const slug = params.slug ? params.slug.join('/') : 'home';
-//   const getData = await fetch(
-//     `http://localhost:3000/api/storyblok?slug=${slug}`
-//   );
-//   const data = await getData.json();
-//   return {
-//     props: {
-//       story: data ? data.story : false,
-//       key: data ? data.story.id : false,
-//     },
-//     revalidate: 20,
-//   };
-// }
+export async function getStaticProps({ params }) {
+  const slug = params.slug ? params.slug.join('/') : 'home';
+  const getData = await fetch(
+    `https://khoros-project.vercel.app/api/storyblok?slug=${slug}`
+  );
+  const data = await getData.json();
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+    },
+    revalidate: 20,
+  };
+}
 
-// export async function getStaticPaths() {
-//   const getPaths = await fetch(`http://localhost:3000/api/storyblok`);
-//   const paths = await getPaths.json();
-//   return {
-//     paths,
-//     fallback: 'blocking',
-//   };
-// }
+export async function getStaticPaths() {
+  const getPaths = await fetch(
+    `https://khoros-project.vercel.app/api/storyblok`
+  );
+  const paths = await getPaths.json();
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
